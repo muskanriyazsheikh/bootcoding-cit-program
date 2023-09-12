30c8ece94f72f529d3249567c0e225608dbe4c94
muskanriyazsheikh <144773065+muskanriyazsheikh@users.noreply.github.com>
2023-09-12 16:03:57 +0530

    Add files via upload
    java codes

---
+++ ArithmeticOperation.java
@@ -0,0 +1,16 @@
+public class ArithmeticOperation
+{
+public static void main(String args[])
+{
+int a=88, b=90;
+int Add,sub,mul,div;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.println("Addition: "+add);
+System.out.println("Substraction: "+sub);
+System.out.println("Multiplication: "+mul);
+System.out.println("Division: "+div);
+}
+}
---
+++ IfElse2.java
@@ -0,0 +1,16 @@
+public class IfElse2{
+public static voi main(String args[])
+int a=20, b=30, c=10;
+if(a>b && a>c){
+System.out.println(a+ "is greater");
+}
+else if(b>a && b>c)
+{
+System.out.println(b + "is greater");
+}else{
+System.out.println(c + "is greater");
+}
+}
+
+
+
